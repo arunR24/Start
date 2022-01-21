ec35f30adec1f59daa1c8b3e5b8b2931838cb746
arun@R24 <karthiarun0123@gmail.com>
2022-01-21 12:26:44 +0530

    hi

---
+++ Github.java
@@ -0,0 +1,13 @@
+package çom.github;
+
+public class Github {
+	private void arun() {
+System.out.println("arun");
+	}
+	private void arun2() {
+System.out.println("arun2");
+	}
+public static void main(String[] args) {
+
+}
+}
